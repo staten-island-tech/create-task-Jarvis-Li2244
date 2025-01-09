5dde2b0ebef3c1ccccaebfb4ae428ec6d24694c6
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-01-09 14:02:44 -0500

    adsdxsafazbfxswTSZy

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,80 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const DOMSelectors = {
-        howtoplay: document.querySelector(".how-to-play")
+        howtoplay: document.querySelector(".how-to-play"),
+        rollbutton: document.querySelector(".roll-button")
+    }
+
+    let player_a_score = {
+        ones: [],
+        twos: [],
+        threes: [],
+        fours: [],
+        fives: [],
+        sixes: [],
+        bonus: [],
+        three_of_a_kind: [],
+        four_of_a_kind: [],
+        full_house: [],
+        small_straight: [],
+        large_straight: [],
+        yahtzee: [],
+        chance: [],
+        total: []
+    }
+
+    let player_b_score = {
+        ones: [],
+        twos: [],
+        threes: [],
+        fours: [],
+        fives: [],
+        sixes: [],
+        bonus: [],
+        three_of_a_kind: [],
+        four_of_a_kind: [],
+        full_house: [],
+        small_straight: [],
+        large_straight: [],
+        yahtzee: [],
+        chance: [],
+        total: []
+    }
+
+    let dice = {
+        dice_1: {
+            value: [],
+            position: "roll"
+        },
+        dice_2: {
+            value: [],
+            position: "roll"
+        },
+        dice_3: {
+            value: [],
+            position: "roll"
+        },
+        dice_4: {
+            value: [],
+            position: "roll"
+        },
+        dice_5: {
+            value: [],
+            position: "roll"
+        }
     }
 
     DOMSelectors.howtoplay.addEventListener("click", function(event) {
         
+    })
+
+    DOMSelectors.rollbutton.addEventListener("click", function(event) {
+
+        for (let i = 0; i < 6; i++) {
+            dice.dice_1.value = [Math.floor(Math.random() * (6 - 1 + 1) ) + 1]
+        }
+        console.log(dice)
     })
 
 
@@ -22,6 +92,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
-
 })
+
